5055c83a56c2feb4b5d5c2667519b150161a1296
mxtokar <mxtokar@gmail.com>
2023-01-04 00:02:21 +0200

    test commit

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#81C8C8",
+      main: "#81C8C7",
     },
     secondary: {
       main: "#fff",
